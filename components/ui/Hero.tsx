eed8995c30e237fd1bbd255f8abf22150d53ec31
Meraj-1 <belalraza158@gmail.com>
2026-07-22 05:06:24 +0530

    update hero

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -30,10 +30,10 @@ const Hero = () => {
     <section className='lg:h-[700px] h-3/5 sm:h-4/5 bg-[#00b8a2] md:flex md:justify-center  text-white '>
       <div className="container md:flex md:flex-row items-center pt-10">
         <div className='flex md:flex-1 justify-center flex-col  items-center transform md:-translate-y-20'>
-          <h1 className={`${londrina.className} text-5xl lg:text-[5rem] xl:text-[8rem] flex`}>Buy perfect <br />for your kid!</h1>
+          <h1 className={`${londrina.className} text-5xl m-2 md:m-0 lg:text-[5rem] xl:text-[5.4rem] flex`}>Inspired by Childhood.<br />Designed for Every Day.</h1>
           <p
             className={`md:text-md text-sm mt-4 text-center md:tracking-[0.2em] xl:text-[1rem] md:font-medium md:mt-10 ${readex.className}`}
-          >Bring happiness to your children with our help. <br /> Purchase interesting toys using a special discount.</p>
+          >From school essentials and creative supplies to premium gifts and everyday accessories, Artiory brings thoughtfully curated products that make every childhood moment memorable.</p>
           <div className={`md:mt-10`}>
             <button className={`bg-white text-2xl font-extrabold cursor-pointer p-10 text-[#00b8a2] px-6 py-2 rounded-full mt-4 ${poppins.className}  hover:bg-gray-200 transition`}>
               Shop Now
