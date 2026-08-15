658f98aaf503eac3a0f14841eb859cdf89d9041e
Meraj-1 <belalraza158@gmail.com>
2026-08-15 11:28:40 +0530

    hide reel sec

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       {/* <FeaturedSec/> */}
       <ShopByCategory/>
       {/* <TrendingReel/> */}
-      <Reels />
+      {/* <Reels /> */}
       <Product/>
       {/* <Subscribe /> */}
       {/* <Footer/> */}
